0b2c609e1d1f3e2a6fc23e7921cb632b49e5cda2
Luis García <luisgasa.uax@gmail.com>
2026-03-02 11:59:12 +0100

    feat (Biblioteca): Creada la clase Biblioteca con constructor y el método findLibro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Biblioteca {
+
+    private Libro [] catalogo;
+    private final int CANT_MAX_LIBROS;
+
+    public Biblioteca(int cantidad_maxima_libros){
+        CANT_MAX_LIBROS = cantidad_maxima_libros;
+        catalogo = new Libro[CANT_MAX_LIBROS];
+    }
+
+    /**
+     * Busca el primer libro que encuentre con el id recibido
+     * @param idLibro
+     * @return
+     */
+    public Libro findLibro(int idLibro){
+        boolean seguirBuscando = true;
+        Libro libroEncontrado = null;
+
+        for (int i = 0; i < CANT_MAX_LIBROS && seguirBuscando; i++) {
+            if( catalogo[i] != null && catalogo[i].getId() == idLibro ){
+                seguirBuscando = false;
+                libroEncontrado = catalogo[i];
+            }
+        }
+
+        return libroEncontrado;
+    }
+
+
+
+
+
+
 }
