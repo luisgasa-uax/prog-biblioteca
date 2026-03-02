7545fdfe9e2d0758132bb90a07092d3674d77c1c
Luis García <luisgasa.uax@gmail.com>
2026-03-02 12:43:56 +0100

    feat (Biblioteca): Pruebas iniciales con el Main

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Biblioteca biblioteca = new Biblioteca( 10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Libro libro1 = new Libro(1, "El Quijote", "Miguel de Cervantes", 678);
+        Libro libro2 = new Libro(2, "El gato con botas", " Charles Perrault", 100);
+
+        System.out.println(biblioteca.findLibro(1) ) ;  // null
+
+        System.out.println(biblioteca.addLibro(libro1)); // true
+        System.out.println(biblioteca.addLibro(libro2)); // true
+
+        System.out.println(biblioteca.obtainCatalogo());
+
     }
 }
